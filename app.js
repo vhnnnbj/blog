96aaf1a905389688cbd661654b4f47ec3bfdfd0b
ubu <harden4@163.com>
2016-09-07 17:15:24 +0800

    chapter 1

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var settings = require('./settings');
+var flash = require('connect-flash');
 var users = require('./routes/users');
 
 var app = express();
@@ -14,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(flash());
 
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
@@ -35,7 +37,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// app.use('/', routes);
+routes(app);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
